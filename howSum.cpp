3dcfeb743cce9a7fca4ec20d7b6d79a5084e2eef
demelcjohn <demal.john007@gmail.com>
2023-04-16 14:59:59 +0530

    hsum

---
+++ howSum.cpp
@@ -0,0 +1,8 @@
+// function howSum(targetSum, numbers)
+// Input: targetSum, array numbers[]
+// Output: An array containing any combination of
+// elements that add up to exactly the targetSum.
+// If no combination that adds up to the targetSum,
+// then return null. If many return any.
+
+#include <iostream>
