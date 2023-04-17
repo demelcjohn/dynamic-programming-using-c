ffe5bd37e2a84547b6ef4efac2498964bdfa1c98
demelcjohn <demal.john007@gmail.com>
2023-04-17 22:22:34 +0530

    canC

---
+++ canConstruct.cpp
@@ -0,0 +1,6 @@
+// Write a function 'canConstruct(target, wordBank)' that
+// accepts a target string and an array of strings.
+// Output: Boolean indicating whether or not the 'target'
+// can be constructed by concatenating elements of the
+//'wordBank' array
+// Reuse of elements of 'wordBank' is possible
