51b784cc788a7a6e972a77c3d45a97053c75d16f
demelcjohn <demal.john007@gmail.com>
2023-04-18 07:00:18 +0530

    canConstruct

--- canConstruct.cpp
+++ canConstruct.cpp
@@ -4,3 +4,45 @@
 // can be constructed by concatenating elements of the
 //'wordBank' array
 // Reuse of elements of 'wordBank' is possible
+
+#include <iostream>
+#include <string.h>
+// #include <map>
+
+using namespace std;
+
+// std::map<int, int> m;
+
+int canConstruct(std::string target, std::string wordBank[50], int n);
+
+int main()
+{
+    int v;
+    std::string target = "skateboard";
+    std::string wordBank[50] = {"bo", "rd", "ate", "t", "ska", "sk", "boar"};
+    int n = 7;
+    v = canConstruct(target, wordBank, n);
+    std::cout << v << std::endl;
+    return 0;
+}
+
+int canConstruct(std::string target, std::string wordBank[50], int n)
+{
+    std::string suffix;
+    if (target == "")
+    {
+        return 1;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        if (target.find(wordBank[i]) == 0)
+        {
+            suffix = target.substr(wordBank[i].length());
+            if (canConstruct(suffix, wordBank, n) == 1)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
