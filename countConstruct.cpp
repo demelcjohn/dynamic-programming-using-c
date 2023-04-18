6800446c2a673bfae270bc10b8e06e1ca65fdc8b
demelcjohn <demal.john007@gmail.com>
2023-04-18 07:17:38 +0530

    cC

---
+++ countConstruct.cpp
@@ -0,0 +1,5 @@
+// function 'countConstruct(target, wordBank)' that accepts a
+// target string and an array of strings.
+// Output: Return number of ways that the target can be constructed
+// by concatenating elements of the wordBank array
+// Reuse of wordBank elements are possible
