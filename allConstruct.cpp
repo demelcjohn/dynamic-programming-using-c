2a430f1d3b4ed3542f10e2fc10a1583053b52ef4
demelcjohn <demal.john007@gmail.com>
2023-04-20 23:16:27 +0530

    allC

---
+++ allConstruct.cpp
@@ -0,0 +1,8 @@
+// function : allConstruct
+// Input : target, wordBank
+// Output : Return 2D array containing all of the ways
+// that the target can be constructed by concatenating
+// elements of wordBank. Each element of 2D array should
+// represent one combination that constructs the target
+// You may reuse elements of wordbank as many times as
+// needed
