e1c61063039204df18cfff770b6d46a7ac8e5c3b
demelcjohn <demal.john007@gmail.com>
2023-04-21 18:18:58 +0530

    all const comm.

--- allConstruct.cpp
+++ allConstruct.cpp
@@ -14,40 +14,51 @@
 
 using namespace std;
 
-std::map<std::string, int> m;
+std::map<std::string, vector<vector<std::string>>> m;
 
-int allConstruct(std::string target, std::string wordBank[50], int n);
+vector<vector<std::string>> allConstruct(std::string target, std::string wordBank[50], int n);
 
 int main()
 {
-    int v;
+    vector<vector<std::string>> v;
     std::string target = "abcdef";
     std::string wordBank[50] = {"ab", "abc", "cd", "def", "abcd", "ef", "cl"};
     int n = 7;
     v = allConstruct(target, wordBank, n);
-    std::cout << v << std::endl;
+    for (vector<std::string> v1 : v)
+    {
+        for (std::string v2 : v1)
+        {
+            std::cout << v2 << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
 
-int allConstruct(std::string target, std::string wordBank[50], int n)
+vector<vector<std::string>> allConstruct(std::string target, std::string wordBank[50], int n)
 {
+
+    vector<vector<std::string>> result{}, suffixWays, targetWays;
     std::string suffix;
-    int totalCount = 0, numWaysForRest;
     if (m.find(target) != m.end())
         return m[target];
     if (target == "")
     {
-        return 1;
+        return vector<vector<std::string>>();
     }
     for (int i = 0; i < n; ++i)
     {
         if (target.find(wordBank[i]) == 0)
         {
             suffix = target.substr(wordBank[i].length());
-            numWaysForRest = allConstruct(suffix, wordBank, n);
-            totalCount += numWaysForRest;
+            suffixWays = allConstruct(suffix, wordBank, n);
+            targetWays = suffixWays.push_back();
+            result.insert(result.end(), targetWays.begin(), targetWays.end());
         }
     }
-    m[target] = totalCount;
-    return totalCount;
+    m[target] = result;
+    return result;
 }
+
+// error not completed
