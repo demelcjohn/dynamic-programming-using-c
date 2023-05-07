270a16117c25a8d94b913238a2ca58e627eb5e37
demelcjohn <demal.john007@gmail.com>
2023-05-07 23:19:08 +0530

    nf-knapsack commit

---
+++ non-frac-knapsack.cpp
@@ -0,0 +1,13 @@
+// 0/1 Knapsack
+
+// We are given N items where each item has some weight
+// and profit associated with it. We are also given a
+// bag with capacity W, [i.e., the bag can hold at most
+// W weight in it]. The target is to put the items into
+// the bag such that the sum of profits associated with
+// them is the maximum possible.
+
+// Note: The constraint here is we can either put an
+// item completely into the bag or cannot put it at all
+// [It is not possible to put a part of an item into
+// the bag].
